31865c8f24f1604b75c872ef8ead3c3f29533aef
dmitry <dmitry@dtech-ural.ru>
2020-05-14 20:33:15 +0500

    описал класс Employee

---
+++ Lesson5/Lesson5/DepartmentObj.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5
+{
+    static class DepartmentObj
+    {
+        internal static int Add(string department)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static string GetValue(int department)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
